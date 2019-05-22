e0e8460493a3432fb903cce81ceda1f99ba30e5d
manoj1 <agarwal.manoj56@yahoo.com>
2019-05-22 13:56:26 -0500

    initial commit

---
+++ src/com/org/binary/CheckBST.java
@@ -0,0 +1,16 @@
+package com.org.binary;
+
+ class Node {
+	int data;
+	Node left,right;
+	public Node(int data) {
+		this.data=data;
+	}
+}
+public class CheckBST {
+
+	public static void main(String[] args) {
+		CheckBST bst = new CheckBST();
+		
+	}
+}
